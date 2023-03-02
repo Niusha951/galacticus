e719374368f991d60a4b2747d0385bb2ec99d865
Niusha Ahvazi <nahvazi@mies.stanford.edu>
2023-03-02 08:12:15 +0000

    Eighth commit: SIDM (failed) test run

---
+++ submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#PBS -N SIDM_test1
+#PBS -l nodes=1:ppn=16
+#PBS -j oe
+#PBS -o outputs/multi_SIDM_test1_16mergerTree.log
+#PBS -V
+#PBS -m abe
+#PBS -M nahvazi@carnegiescience.edu
+
+cd $PBS_O_WORKDIR
+export OMP_THREAD_NUM=16
+/usr/bin/time -v Galacticus.exe parameters/myParams/multi_SIDM_test_16mergerTree.xml
+
+
