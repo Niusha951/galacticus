6924a99b60982018b490a90a00c44f0c926ac767
Andrew Benson <abenson@carnegiescience.edu>
2023-04-07 16:07:30 -0700

    fix(perf): Add `save` attribute to avoid expensive finalization

--- source/numerical.interpolation.F90
+++ source/numerical.interpolation.F90
@@ -517,7 +517,8 @@ contains
     double precision                                  , parameter                   :: rangeTolerance   =1.0d-6
     type            (enumerationExtrapolationTypeType)                              :: extrapolationType
     integer         (c_size_t                        )                              :: basePoint
-    type            (varying_string                  )                              :: message
+    type            (varying_string                  ), save                        :: message
+    !$omp threadprivate(message)
     integer         (c_int                           )                              :: statusGSL
     double precision                                                                :: gradient                , x_
     character       (len=12                          )                              :: labelX                  , labelX_      , &
